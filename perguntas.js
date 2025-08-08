32ce234efd6a4aae4f0d796b276d3eee4eeee71a
sasadlzt <sarah.dalzoto@escola.pr.gov.br>
2025-08-08 12:08:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Filmes',  
+    'Qual o melhor filme de romance?', 
+    'Como Eu Era Antes de Você'  
+ 
+)
+ criaCartao(
+    'Praia', 
+    'Qual o lugar do Brasil tem as praias mais lindas?', 
+    'Baía do Sancho – Fernando de Noronha, Brasil' 
+ )
+ 
+ 
+ criaCartao(
+    'Comida',  
+    'Qual é a comida mais gostosa do Brasil?', 
+    'Feijoada' 
+ )
+ 
+ 
+ criaCartao(
+    'Freddie Mercury',
+    'Qual a melhor música do Freddie Mercury?', 
+    'Bohemian Rhapsody' 
+ )
